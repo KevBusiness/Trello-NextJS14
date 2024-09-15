5cfa5881a350bae846d72ffb1b600373e8b2562f
KevinInCode <kevinfelix.bussines@gmail.com>
2024-09-14 21:03:34 -0700

    add sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import {OrganizationSwitcher, UserButton} from "@clerk/nextjs";
+import {Plus} from "lucide-react";
 import Logo from "@/components/logo";
 import {Button} from "@/components/ui/button";
-import {Plus} from "lucide-react";
-import {OrganizationSwitcher, UserButton} from "@clerk/nextjs";
+import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
   return (
     <nav className="fixed z-50 top-0 w-full h-14 border-b shadow-sm bg-white flex items-center">
-      {/*TODO: Mobile sidebar*/}
+      <MobileSidebar />
       <div className="flex items-center gap-x-4">
         <div className="hidden md:flex">
           <Logo />
